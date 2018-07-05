54d865e9fefac302269cfcda4d8e90e2342c648a
cryptofox3 <pghodgson558@gmail.com>
2018-07-05 14:32:26 -0500

    Added 'add' method

---
+++ MyListTests/MyListTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList.Tests
+{
+    [TestClass()]
+    public class MyListTests
+    {
+        [TestMethod()]
+        public void 
+            ()
+        {
+            Assert.Fail();
+        }
+    }
+}
