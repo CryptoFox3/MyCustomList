4b0d8c917d8a8b75482b161a93846af33c596864
cryptofox3 <pghodgson558@gmail.com>
2018-07-05 10:06:04 -0500

    Initial Commit

---
+++ MyList/MyList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    public class MyList
+    {
+
+        public CreateCustomList 
+            {
+            CustomList = new <T>;
+            }
+
+
+
+
+    }
+}
---
+++ MyListTest/AddTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MyList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyListTest
+{
+    [TestClass]
+    public class AddTest
+    {
+        [TestMethod]
+        public void AddOneIndex()
+        {
+           
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+        }
+    }
+}
