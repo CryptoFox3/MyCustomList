41684943f6c7b9f9b2326d0d914cc252ab14fa6e
phodgson558 <pghodgson558@gmail.com>
2018-10-12 14:40:29 -0500

    Added ZipperTwoStringLists test

--- MyListTests/MyListTestZipper.cs
+++ MyListTests/MyListTestZipper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyList;
 
 namespace MyListTests
 {
@@ -7,8 +8,30 @@ namespace MyListTests
     public class MyListTestZipper
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ZipperTwoStringLists()
         {
+            // Arrange
+            MyList<string> myListOne = new MyList<string>();
+            MyList<string> myListTwo = new MyList<string>();
+            
+            // Act
+            myListOne.Add("x");
+            myListOne.Add("y");
+            myListOne.Add("z");
+            myListTwo.Add("a");
+            myListTwo.Add("b");
+            myListTwo.Add("c");
+
+            MyList<string> MyListThree = myListOne.Zip(myListTwo);
+
+
+            // Assert
+            Assert.AreEqual(MyListThree[0], "x");
+            Assert.AreEqual(MyListThree[1], "a");
+            Assert.AreEqual(MyListThree[2], "y");
+            Assert.AreEqual(MyListThree[3], "b");
+            Assert.AreEqual(MyListThree[4], "z");
+            Assert.AreEqual(MyListThree[5], "c");
         }
     }
 }
